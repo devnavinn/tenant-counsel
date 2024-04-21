6ba1d87afec5d169b4cd3f51fee43e96272de66b
neo062 <navinkumar241999@gmail.com>
2024-04-21 21:44:30 +0530

    changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import logo from './../assets/logo.svg'
 import { Link } from 'react-router-dom'
 const Footer = () => {
     return (
-        <footer className="flex justify-between items-center px-10 py-2 mt-5 shadow border-t">
+        <footer className="flex justify-between items-center px-10 py-2 mt-10 shadow border-t">
             <Link to={'/'} className='flex items-center w-16 h-16 '>
                 <img src={logo} alt="logo" className='w-full h-full' />
             </Link>
