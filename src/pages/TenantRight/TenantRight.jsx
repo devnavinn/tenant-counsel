6df6bb890f9a46510e2a41eabbcfaa2b18e2ff70
neo062 <navinkumar241999@gmail.com>
2024-04-22 18:41:25 +0530

    changes

--- src/pages/TenantRight/TenantRight.jsx
+++ src/pages/TenantRight/TenantRight.jsx
@@ -1,4 +1,6 @@
+
 import Hero from "./Hero"
+import { Link } from "react-router-dom";
 import { useRef } from "react"
 import Illegal from "./Illegal";
 import Discrimination from "./Discrimination";
@@ -9,49 +11,72 @@ import SafteyAndRepair from "./SafteyAndRepair";
 import RentStabilation from "./RentStabilation";
 import Help from "../../components/Help";
 const TenantRight = () => {
-    const currentSectionRef = useRef('Home');
-
-    const navigateToSection = (section) => {
-        currentSectionRef.current = section;
-        console.log("Navigated to section:", section);
+    const homeRef = useRef(null);
+    const illegalRef = useRef(null);
+    const landlordRef = useRef(null);
+    const heatRef = useRef(null);
+    const safetyRef = useRef(null);
+    const rentRef = useRef(null);
+    const discriminationRef = useRef(null);
+    const disabilityRef = useRef(null);
+    const navigateToSection = (sectionRef) => {
+        sectionRef.current.scrollIntoView({ behavior: "smooth" });
     };
+
     return (
         <>
             <Hero />
-            {/* <ul className="flex flex-row justify-center gap-2 flex-wrap">
-                <li className="cursor-pointer text-blue-500 hover:text-blue-700" onClick={() => navigateToSection('Home')}>
-                    Home
+            <ul className="flex flex-row justify-center gap-2 flex-wrap my-5">
+                <li className="cursor-pointer text-gray-700" >
+                    <Link to={'/'} > Home</Link>
                 </li>
-                <li className="border-r border-gray-200 cursor-pointer text-blue-500 hover:text-blue-700" onClick={() => navigateToSection('Illegal lockouts and Eviction')}>
+                <li className="border-r pr-2 border-gray-900  cursor-pointer text-gray-700" onClick={() => navigateToSection(illegalRef)}>
                     Illegal lockouts and Eviction
                 </li>
-                <li className="border-r border-gray-200 cursor-pointer text-blue-500 hover:text-blue-700" onClick={() => navigateToSection('Landlord Harrasment')}>
-                    Landlord Harrasment
+                <li className="border-r pr-2 border-gray-900  cursor-pointer text-gray-700" onClick={() => navigateToSection(landlordRef)}>
+                    Landlord Harassment
                 </li>
-                <li className="border-r border-gray-200 cursor-pointer text-blue-500 hover:text-blue-700" onClick={() => navigateToSection('Heat and Hot Water')}>
+                <li className="border-r pr-2 border-gray-900  cursor-pointer text-gray-700" onClick={() => navigateToSection(heatRef)}>
                     Heat and Hot Water
                 </li>
-                <li className="border-r border-gray-200 cursor-pointer text-blue-500 hover:text-blue-700" onClick={() => navigateToSection('Safety and Repairs')}>
+                <li className="border-r pr-2 border-gray-900  cursor-pointer text-gray-700" onClick={() => navigateToSection(safetyRef)}>
                     Safety and Repairs
                 </li>
-                <li className="border-r border-gray-200 cursor-pointer text-blue-500 hover:text-blue-700" onClick={() => navigateToSection('Rent Stabilization')}>
+                <li className="border-r pr-2 border-gray-900  cursor-pointer text-gray-700" onClick={() => navigateToSection(rentRef)}>
                     Rent Stabilization
                 </li>
-                <li className="border-r border-gray-200 cursor-pointer text-blue-500 hover:text-blue-700" onClick={() => navigateToSection('Discrimination')}>
+                <li className="border-r pr-2 border-gray-900  cursor-pointer text-gray-700" onClick={() => navigateToSection(discriminationRef)}>
                     Discrimination
                 </li>
-                <li className="border-r border-gray-200 cursor-pointer text-blue-500 hover:text-blue-700" onClick={() => navigateToSection('Disability Protections')}>
+                <li className="border-r pr-2 border-gray-900  cursor-pointer text-gray-700" onClick={() => navigateToSection(disabilityRef)}>
                     Disability Protections
                 </li>
-            </ul> */}
+            </ul>
             <div className="px-5 flex flex-col space-y-10">
-                <Illegal />
-                <Discrimination />
-                <DisabilitProtection />
-                <LandLord />
-                <HeatAndHot />
-                <SafteyAndRepair />
-                <RentStabilation />
+                <div ref={illegalRef}>
+                    <Illegal />
+                </div>
+                <div ref={discriminationRef}>
+                    <Discrimination />
+                </div>
+                <div ref={disabilityRef} >
+                    <DisabilitProtection />
+                </div>
+                <div ref={landlordRef}>
+                    <LandLord />
+                </div>
+                <div ref={heatRef}>
+                    <HeatAndHot />
+                </div>
+                <div ref={safetyRef}>
+                    <SafteyAndRepair />
+                </div>
+                <div ref={rentRef}>
+                    <RentStabilation />
+                </div>
+                <div ref={rentRef}>
+                    <RentStabilation />
+                </div>
                 <Help />
             </div>
         </>
