46f148a8bebad0f77ab9b5c4b8d9fb3367430262
neo062 <navinkumar241999@gmail.com>
2024-04-25 17:02:49 +0530

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,20 @@ import WhyWeAre from "../components/WhyWeAre"
 import Demo from "../components/Demo"
 import Support from "../components/Support"
 import Help from "../components/Help"
+import { Helmet } from "react-helmet"
 const Home = () => {
     return (
         <section
             className="overflow-hidden"
         >
+            <Helmet>
+                <title>Tenant Right </title>
+                <meta
+                    name="description"
+                    content="Tenant Right is a platform that helps tenants to know their rights and responsibilities. We provide information about the rights and responsibilities of tenants in the United States."
+                />
+            </Helmet>
+
             <Hero />
             <div className="px-5 flex flex-col gap-10">
                 <WhyWeAre />
