9707089fe7130b5dd5c3e0d3145c8c83b4d29bb1
neo062 <navinkumar241999@gmail.com>
2024-05-04 15:19:24 +0530

    img size

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import heroImg from '../assets/hero-img.svg';
+import heroImg from '../assets/hero-img.png';
 import shapeHero from '../assets/shape-hero.svg';
 import shapeHero2 from '../assets/shape-hero2.png';
 import { motion } from "framer-motion";
